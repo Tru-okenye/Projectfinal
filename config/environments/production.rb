920bdf3c4a8800f18d8c82ba5c1a83cfb076f2ed
Truphy <truphenaokenye@gmail.com>
2023-07-21 15:25:07 +0300

    made few changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,20 @@
 require "active_support/core_ext/integer/time"
 
+
+
 Rails.application.configure do
+
+  
+# config/application.rb or config/initializers/cors.rb
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://127.0.0.1:5173'  # Replace this with the actual URL of your frontend
+    resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+
+  
+
   # Settings specified here will take precedence over those in config/application.rb.
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
