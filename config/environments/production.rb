8006d2bcc1b7a8812a6863f97543201e8db243a6
Truphy <truphenaokenye@gmail.com>
2023-07-23 18:09:01 +0300

    get

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ Rails.application.configure do
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://127.0.0.1:5173'  # Replace this with the actual URL of your frontend
-    resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+    exclude: ['/api/callback']
   end
 end
 
