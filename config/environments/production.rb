490c19680ea0609bb2b04d469448734937aded10
Truphy <truphenaokenye@gmail.com>
2023-08-25 20:51:29 +0300

    more changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 # config/application.rb or config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://127.0.0.1:5173'  # Replace this with the actual URL of your frontend
+    origins 'https://bus-booking-frontend-swart.vercel.app/'  # Replace this with the actual URL of your frontend
     resource '/api/*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
